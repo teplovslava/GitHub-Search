097fce8b1e04be9e2531db5d575196b942f2975a
Slava <slava.teplov.2014@mail.ru>
2022-07-07 13:01:39 +0300

    disign and search after click button are added

--- dist/script.js
+++ dist/script.js
@@ -2,40 +2,54 @@ let inputSearch = document.querySelector('.inputSearch')
 let vals = inputSearch.value
 let res = document.querySelector(".result")
 let addBtn = document.querySelector(".container button")
+let h3 = document.querySelector(".resH3")
 
 
 addBtn.addEventListener('click',function(){
+    res.innerHTML=""
+    h3.innerHTML=""
+
+
 searchRepos()
 
 })
 
 
 function searchRepos(){
-    setTimeout(
-        ( fetch(`https://api.github.com/search/repositories?q=${inputSearch.value}&sort=stars&order=desc`)
+        fetch(`https://api.github.com/search/repositories?q=${inputSearch.value}&sort=stars&order=desc`)
     .then(result=>result.json())
     .then((data)=>{data.items.forEach(element => {
-        console.log(data.total_count)
+        console.log(element)
         let div = document.createElement('div')
-        let h3=document.createElement('h3')
-        h3.innerHTML=`Найдено ${data.total_count} репозиториев`
+        div.className="elemDiv"
+        let div2 = document.createElement('div')
+        div2.className="elemDiv2"
+        let div3 = document.createElement('div')
+        div3.className="elemDiv3"
         let link = document.createElement('a')
+        let img = document.createElement('img')
+        img.className="avatar"
         link.setAttribute("href",element.html_url)
         link.setAttribute("target","_blank")
+        img.setAttribute("src",element.owner.avatar_url)
         link.innerHTML=element.name
-        div.append(link)
+        link.className="reposLink"
+        let p=document.createElement('a')
+        p.innerHTML=`Автор: ${element.owner.login}`
+        p.setAttribute("href",element.owner.html_url)
+        p.setAttribute("target","_blank")
+        p.className="autorLink"
+        div3.innerHTML=`${element.stargazers_count} &#127775`
+        div.append(img)
+        div.append(div2)
+        div.append(div3)
+        div2.append(link)
+        div2.append(p)
         res.append(div)
     })
 
-    let h3=document.createElement('h3')
-    h3.innerHTML=`Найдено ${data.total_count} результатов`
-    res.append(h3)
-        
-        
-    
-        
-               
-            })),1000)
+    h3.innerHTML=`Найдено ${data.total_count} репозиториев`             
+            })
          
         }
 
